c924b191cd97370d3ffe1c49a9eb5201cef3e6b3
sirsuccess <kanuamani@gmaail.com>
2022-04-15 17:22:53 +0100

    work on navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss';
+
+import Logo from '../../assets/images/logo.svg';
+
+const Benefits = () => {
+  return (
+    <div className='navbar container'>
+      <div className="logo">
+        <img src={Logo} alt="SDG Logo" />
+        </div>
+    </div>
+  );
+};
+
+export default Benefits;
