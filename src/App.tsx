c2beac0f518f55cfe1b6db22447bc134f2a63a73
sirsuccess <kanuamani@gmaail.com>
2022-04-15 17:23:22 +0100

    make changes to other sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
-import Benefits from './components/benefits';
-import Newsletter from './components/newsletter';
+import React from "react";
+import "./App.css";
+import Benefits from "./components/benefits";
+import Newsletter from "./components/newsletter";
+import Navber from "./components/navbar";
+import Hero from "./components/hero";
+import Footer from "./components/footer"
 
 function App() {
   return (
     <>
+      <Navber />
+      <Hero />
       <Benefits />
       <Newsletter />
+      <Footer/>
     </>
   );
 }
