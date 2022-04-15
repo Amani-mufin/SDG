cb7c0f41f165f81ba521644bdce19467c8300574
sirsuccess <kanuamani@gmaail.com>
2022-04-15 17:22:38 +0100

    woork oon footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.scss';
+
+import GoogleIcon from '../../assets/images/google-icon.svg';
+
+const Benefits = () => {
+  return (
+    <div className='footer container text-center' >
+      <div className="header mb-2">
+      Powered By
+        </div>
+        <div>
+          <img src={GoogleIcon} alt="google developer group"  className="mr-5"/> Google Developer Groups
+        </div>
+        <div>Ajah</div>
+    </div>
+  );
+};
+
+export default Benefits;
