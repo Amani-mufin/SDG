f293f3a5c423cb2962f3b543f8653c67301fa624
sirsuccess <kanuamani@gmaail.com>
2022-06-14 14:54:02 +0100

    new changes

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -7,7 +7,7 @@ import Playstore from "../../assets/images/playstore-icon.svg";
 const Hero = () => {
   return (
     <div className="hero_container container">
-      <p className="heading">Coming Soon</p>
+      <p className="heading">SDG Health</p>
       <p className="subtitle col-md-5 text-center mx-auto mb-5">
         SDG Health Education Mobile App aims to encourage people to adopt and
         sustain health promoting life style and practices
@@ -18,19 +18,19 @@ const Hero = () => {
           <div className="left ">
             <img src={Playstore} alt="play store" />
           </div>
-          <div className="right">
-            <div>Coming soon on</div>
+          <a className="right" href="https://play.google.com/store/apps/details?id=com.gdgajah.sdg_flutter" target="_blank"  rel="noreferrer" >
+            <div>Get it on</div>
             <div className="title">Google Play</div>
-          </div>
+          </a>
         </button>
         <button className="d-flex  mt-3 mt-md-0 justify-content-between align-items-center">
           <div className="left">
             <img src={AppleStore} alt="play store" />
           </div>
-          <div className="right">
-            <div>Coming soon on</div>
+          <a className="right" href="https://play.google.com/store/apps/details?id=com.gdgajah.sdg_flutter" target="_blank"  rel="noreferrer">
+            <div>Get it on</div>
             <div className="title">App Store</div>
-          </div>
+          </a>
         </button>
       </div>
     </div>
